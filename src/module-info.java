e8e5e392d67b41b5328c195d9cc22281b2f1b03d
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-10-20 23:50:00 -0400

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module EmailClient {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.bwinchester;
+    opens com.bwinchester.view;
+}
